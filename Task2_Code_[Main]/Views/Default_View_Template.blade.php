a0c9e9eff16bd5a0686616e52781984f999883d2
Exam291 <exam291@truro-penwith.ac.uk>
2022-03-09 10:40:42 +0000

    first commit 9 march

--- Task2_Code_[Main]/Views/Default_View_Template.blade.php
+++ Task2_Code_[Main]/Views/Default_View_Template.blade.php
@@ -1,38 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-        <!-- <link rel="stylesheet" href="Home_Page_Styles.css"> -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <link rel="stylesheet" href="Home_Page_Styles.css">
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Home Page</title>
+        @yield('title')
     </head>
     <header>
-        <nav class="navbar navbar-expand-lg">
+        <nav class="navbar navbar-expand-sm bg-primary navbar-dark">
             <div class="container-fluid">
-                <li class="nav-item">
-                    <a href="index.php">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a href="index.php">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a href="index.php">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a href="index.php">Home</a>
-                </li>
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php" style="margin-left: 5vw;">Home</a>
+                    </li>
+                </ul>
+                <a class="navbar-brand ms-auto">GibJohn Tutorials</a>
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="Login_Page.php">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="Signup_Page.php" style="margin-right: 5vw;">Signup</a>
+                    </li>
+                </ul>
             </div>
         </nav>
     </header>
     <body>
         @yield('content')
         <br>
+        <div class="footer">
+            <p>footer</p>
+        </div>
     </body>
-    <footer>
-        Footer
-    </footer>
 </html>
--- Task2_Code_[Main]/Views/Home_Page.blade.php
+++ Task2_Code_[Main]/Views/Home_Page.blade.php
@@ -1,2 +1,10 @@
 @extends('Default_View_Template')
-<?php
+
+@section('title')
+<title>Home Page</title>
+@endsection
+@section('content')
+
+<p>hello World</p>
+
+@endsection
